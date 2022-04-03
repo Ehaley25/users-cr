1007d8604cd90588d1a7751e339f3b46d7f34ca0
Elijah Haley <elijahhaley99@gmail.com>
2022-04-03 12:53:04 -0700

    fully functional CRUD

--- server.py
+++ server.py
@@ -1,29 +1,57 @@
-from socket import create_server
-from flask import Flask,render_template,request, redirect
+from flask import Flask, render_template, request, redirect
+
 from users import User
-app = Flask(__name__)
+
+app=Flask(__name__)
 
 @app.route('/')
 def index():
-    users = User.show()
-    print(users)
-    return render_template("index.html", all_users = users)
+    return redirect('/users')
 
 
-@app.route('/create' ,methods = ["POST"])
+@app.route('/users')
+def users():
+    return render_template("users.html",users=User.get_all())
+
+
+@app.route('/user/new')
+def new():
+    return render_template("new_user.html")
+
+@app.route('/user/create',methods=['POST'])
 def create():
-    data = {
-        'fname':request.form['fname'],
-        'lname':request.form['lname'],
-        'emails':request.form['emails']
+    print(request.form)
+    User.save(request.form)
+    return redirect('/users')
+
+
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data ={ 
+        "id":id
     }
+    return render_template("edit_user.html",user=User.get_one(data))
 
-    User.insert(data)
-    return redirect('/show')
+@app.route('/user/show/<int:id>')
+def show(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("show_user.html",user=User.get_one(data))
 
-@app.route('/create')
-def show():
-    return render_template('create.html')
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data ={
+        'id': id
+    }
+    User.destroy(data)
+    return redirect('/users')
 
 if __name__=="__main__":
     app.run(debug=True)
--- users.py
+++ users.py
@@ -1,8 +1,7 @@
 from mysqlconnection import connectToMySQL
 
-
 class User:
-    def __init__(self,data):
+    def __init__(self, data):
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
@@ -10,21 +9,40 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
+
     @classmethod
-    def show(cls):
-        query = "select * from users"
-
-        results = connectToMySQL("users_schema").query_db(query)
-
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL('users_schema').query_db(query)
         users = []
-
-        for user in results:
-            users.append(cls(user))
+        for u in results:
+            users.append( cls(u) )
         return users
 
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users (first_name,last_name,email) VALUES (%(first_name)s,%(last_name)s,%(email)s);"
+
+        # comes back as the new row id
+        result = connectToMySQL('users_schema').query_db(query,data)
+        return result
 
     @classmethod
-    def insert(cls,data):
-        query = "insert into users(first_name , last_name , email , created_at , updated_at) values(%(fname)s ,%(lname)s , %(emails)s , NOW(), NOW() )"
+    def get_one(cls,data):
+        query  = "SELECT * FROM users WHERE id = %(id)s ;"
+        result = connectToMySQL('users_schema').query_db(query,data)
+        return cls(result[0])
 
-        return connectToMySQL("users_schema").query_db(query,data)
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE users SET first_name=%(first_name)s,last_name=%(last_name)s,email=%(email)s,updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
+
+    @classmethod
+    def destroy(cls,data):
+        query  = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
